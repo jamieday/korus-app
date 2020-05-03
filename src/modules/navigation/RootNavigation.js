a4e31e521b1eb47cb041e74374b9de367fa44f2e
Jamie Day <jamie@jamieday.ca>
2020-05-03 16:23:53 -0700

    fix misc

--- src/modules/navigation/RootNavigation.js
+++ src/modules/navigation/RootNavigation.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import { Image, TouchableOpacity } from 'react-native';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
-
 import MainTabNavigator from './MainTabNavigator';
-
-import GalleryScreen from '../gallery/GalleryViewContainer';
-
-// To use this screens please see the full version at https://reactnativestarter.com
-// import ProfileScreen from '../containers/ProfileScreen';
-// import ArticleScreen from '../containers/ArticleScreen';
-// import ChatScreen from '../containers/chat/ChatScreen';
-// import MessagesScreen from '../containers/chat/MessagesScreen';
-// import ChartsScreen from '../containers/ChartsScreen';
-
-import AvailableInFullVersion from '../availableInFullVersion/AvailableInFullVersionViewContainer';
-
 import { colors, fonts } from '../../styles';
 
 const stackNavigator = createStackNavigator(
@@ -35,42 +22,6 @@ const stackNavigator = createStackNavigator(
         //   />
         // ),
       }),
-    },
-    Profile: {
-      screen: AvailableInFullVersion,
-      navigationOptions: {
-        header: null,
-      },
-    },
-    Gallery: {
-      screen: GalleryScreen,
-      navigationOptions: {
-        title: 'Gallery',
-      },
-    },
-    Article: {
-      screen: AvailableInFullVersion,
-      navigationOptions: {
-        header: null,
-      },
-    },
-    Chat: {
-      screen: AvailableInFullVersion,
-      navigationOptions: {
-        header: null,
-      },
-    },
-    Messages: {
-      screen: AvailableInFullVersion,
-      navigationOptions: {
-        header: null,
-      },
-    },
-    Charts: {
-      screen: AvailableInFullVersion,
-      navigationOptions: {
-        header: null,
-      },
     },
   },
   {
